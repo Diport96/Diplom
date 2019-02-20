1aacc18c1dcb51b2317ff5f09a53fc60536a520f
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-02-20 04:59:58 +0300

    Commit after create the switch class

---
+++ DiplomApp/Controllers/Switch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomApp.Controllers
+{
+    class Switch : Controller
+    {
+        private bool value;
+        public bool Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public Switch(string id, string name, bool value) : base(id, name)
+        {
+            Value = value;
+        }
+    }
+}
