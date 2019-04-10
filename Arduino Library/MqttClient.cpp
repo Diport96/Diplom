ded03f92cc449302ee8663400145630814785948
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-04-10 17:27:05 +0300

    Intermediate commit 2

--- Arduino Library/MqttClient.cpp
+++ Arduino Library/MqttClient.cpp
@@ -1,16 +1,17 @@
 #include "MqttClient.h"
 #include "PubSubClient.h"
 #include "Client.h"
-#include "ArduinoJson-v6.10.0.h"
+#include "ArduinoJson-v5.13.4.h"
 
 
-MqttClientSensor::MqttClientSensor( const char* id, const char* name, Client& client, double& sensorVal, const char* sensorType, IPAddress ip, uint16_t port)
+MqttClientSensor::MqttClientSensor(const char* id, const char* name, Client& client, double& sensorVal, const char* sensorType, IPAddress ip, uint16_t port, const char* mqttTopic)
 {    
-    this->ID = id;
-    this->Name = name;
-    this->Value = sensorVal;  
-    this->Type = sensorType;
+    this->id = id;
+    this->name = name;
+    this->value = sensorVal;  
+    this->type = sensorType;
     this->connected = false;
+    this->topic = mqttTopic;
     this->_client = new PubSubClient(ip, port, client);      
 }
 
@@ -18,14 +19,43 @@ bool MqttClientSensor::Connect()
 {
     // Попытка подключения
     if(!_client->connect(this->ID)) return false;
+    _client.subscribe(TOPIC_FOR_CONNECTION);
 
     // Формироваие JSON документа
-    sensorDoc["ID"] = this->ID;
-    sensorDoc["Name"] = this->Name;
-	sensorDoc["Value"] = this->Value;
-    resultDoc["Message_Type"] = REQUEST_TO_CONNECT;
-	resultDoc["Type"] = this->Type;
-	resultDoc["Class"] = sensorDoc;
+    sensorObj["ID"] = this->id;
+    sensorObj["Name"] = this->name;
+	sensorObj["Value"] = this->value;
+    resultObj["Message_Type"] = REQUEST_TO_CONNECT;
+	resultObj["Type"] = this->type;
+	resultObj["Class"] = sensorDoc;
 
-    // !!!
+    // Конвертирование в строку
+    const char* res;
+    resultObj.prettyPrintTo(res);
+
+    // Отправка сообщения на сервер
+    _client.publish(topic, res);
+}
+
+void MqttClientSensor::callback(char* topic, byte* payload, unsigned int length)
+{
+    if(topic == TOPIC_FOR_CONNECTION)
+    {
+        char[length] message;
+        for(int i = 0; i < length; i++)
+        {
+            message[i] = (char)payload[i];
+        }
+
+        if(message == PERMIT_TO_CONNECT)
+        {
+            //!!! namespace?
+            MqttClientSensor::connected = true;
+        }
+    }
+}
+
+bool MqttClientSensor::distributeValues()
+{
+    //
 }
--- Arduino Library/MqttClient.h
+++ Arduino Library/MqttClient.h
@@ -3,34 +3,38 @@
 
 #include "PubSubClient.h"
 #include "Client.h"
-#include "ArduinoJson-v6.10.0.h"
+#include "ArduinoJson-v5.13.4.h"
 
-#define REQUEST_TO_CONNECT "CONNECTION"
+#define REQUEST_TO_CONNECT "CONNECT"
 #define PERMIT_TO_CONNECT "CONNACK"
 #define REQUEST_TO_DISCONNECT "DISCONNECT"
+
+#define TOPIC_FOR_CONNECTION = "Connection"
 #endif
 
 class MqttClientSensor {
 private:    
-    const char* ID;
-	const char* Name;
-	const char* Type;
-	double Value;    
+    const char* id;
+	const char* name;
+	const char* type;
+	double value;    
     bool connected;
+    const char* topic;
     PubSubClient* _client;    
 
     //Json параметры
     const int capacity = JSON_OBJECT_SIZE(12);
-    StaticJsonDocument<capacity> sensorDoc; // Данные об подключаемом усторйстве
-    StaticJsonDocument<capacity> resultDoc;
+    StaticJsonBuffer<capacity> jb;
+    JsonObject& sensorObj = jb.createObject();
+	JsonObject& resultObj = jb.createObject();
 
+    //Методы
+    void callback(char* topic, byte* payload, unsigned int length);
+    bool distributeValues(); // Not implemented
 
 public:     
-    MqttClientSensor(const char*, const char*, Client& client, double&, const char*, IPAddress, uint16_t); 
-        
-
-    bool Connect(); //Not implemented
-    void Run(); //Not implemented
+    MqttClientSensor(const char*, const char*, Client& client, double&, const char*, IPAddress, uint16_t, const char*); 
+    bool Connect();   
 };
 
 
