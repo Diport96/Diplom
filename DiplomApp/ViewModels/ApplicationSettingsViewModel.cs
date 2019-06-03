393d62e9e6deb8fa82f98e9359562d30c8e0d812
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-06-03 19:55:02 +0300

    View-Model for application settings is created

---
+++ DiplomApp/ViewModels/ApplicationSettingsViewModel.cs
@@ -0,0 +1,113 @@
+﻿using DiplomApp.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace DiplomApp.ViewModels
+{
+    class ApplicationSettingsViewModel : INotifyPropertyChanged
+    {
+        private readonly Window owner;
+        private bool autoSendData;
+        private bool enableDebugInfo;
+        private string webAppUrl;
+        private string selectedAutoSendDataTime;
+        private RelayCommand submitChangesCommand;
+        private RelayCommand cancelChangesCommand;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Dictionary<string, TimeSpan> AutoSendDataEvery { get; }
+            = new Dictionary<string, TimeSpan>()
+        {
+            { "30 минут", new TimeSpan(0, 30, 0) },
+            { "1 час", new TimeSpan(1, 0, 0) },
+            { "2 часа", new TimeSpan(2, 0, 0) },
+            { "5 часов", new TimeSpan(5, 0, 0) },
+            { "16 часов", new TimeSpan(16, 0, 0) },
+            { "1 день", new TimeSpan(1, 0, 0, 0) },
+            { "3 дня", new TimeSpan(3, 0, 0, 0) },
+            { "1 неделя", new TimeSpan(7, 0, 0, 0) }
+        };
+        public bool AutoSendData
+        {
+            get { return autoSendData; }
+            set
+            {
+                autoSendData = value;
+                OnPropertyChanged("AutoSendData");
+            }
+        }
+        public string SelectedAutoSendDataTime
+        {
+            get { return selectedAutoSendDataTime; }
+            set
+            {
+                selectedAutoSendDataTime = value;
+                OnPropertyChanged("SelectedAutoSendDataTime");
+            }
+        }
+        public bool EnableDebugInfo
+        {
+            get { return enableDebugInfo; }
+            set
+            {
+                enableDebugInfo = value;
+                OnPropertyChanged("EnableDebugInfo");
+            }
+        }
+        public string WebAppUrl
+        {
+            get { return webAppUrl; }
+            set
+            {
+                webAppUrl = value;
+                OnPropertyChanged("ServerUrl");
+            }
+        }
+        public RelayCommand SubmitChangesCommand
+        {
+            get
+            {
+                return submitChangesCommand ??
+                    (submitChangesCommand = new RelayCommand(obj => SubmitChanges()));
+            }
+        }
+        public RelayCommand CancelChangesCommand
+        {
+            get
+            {
+                return cancelChangesCommand ??
+                    (cancelChangesCommand = new RelayCommand(obj => CancelChanges()));
+            }
+        }
+
+        public ApplicationSettingsViewModel(Window owner)
+        {
+            this.owner = owner;
+            WebAppUrl = Properties.Settings.Default.WebAppUrl;
+            AutoSendData = Properties.Settings.Default.AutoSendDataToWebApp;
+            EnableDebugInfo = Properties.Settings.Default.EnableDebugInfo;
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        private void SubmitChanges()
+        {
+            Properties.Settings.Default.WebAppUrl = WebAppUrl;
+            Properties.Settings.Default.AutoSendDataToWebApp = AutoSendData;
+            if (!string.IsNullOrEmpty(SelectedAutoSendDataTime)) Properties.Settings.Default.AutoSendDataEvery = AutoSendDataEvery[SelectedAutoSendDataTime];
+            Properties.Settings.Default.EnableDebugInfo = EnableDebugInfo;
+
+            Properties.Settings.Default.Save();
+            owner.DialogResult = true;
+        }
+        private void CancelChanges()
+        {
+            owner.DialogResult = false;
+        }
+    }
+}
--- DiplomApp/Views/ApplicationSettingsWindow.xaml.cs
+++ DiplomApp/Views/ApplicationSettingsWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DiplomApp.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DiplomApp.Views
 {
@@ -25,63 +14,7 @@ namespace DiplomApp.Views
         public ApplicationSettingsWindow()
         {
             InitializeComponent();
-
-            WebAppAddressTextBox.Text = Properties.Settings.Default.WebAppUrl;
-            AutoSendDataCheckBox.IsChecked = Properties.Settings.Default.AutoSendDataToWebApp;
-            EnableDebugInfoCheckBox.IsChecked = Properties.Settings.Default.EnableDebugInfo;
-
-            AutoSendDataCheckBox_CheckChanged(this, null);
-            AutoSendDataCheckBox.Checked += AutoSendDataCheckBox_CheckChanged;
-            AutoSendDataCheckBox.Unchecked += AutoSendDataCheckBox_CheckChanged;
-        }
-
-        private void AutoSendDataCheckBox_CheckChanged(object sender, RoutedEventArgs e)
-        {
-            if (AutoSendDataCheckBox.IsChecked.HasValue)
-            {
-                if (AutoSendDataCheckBox.IsChecked.Value)
-                    AutoSendDataSettings.IsEnabled = true;
-                else if (!AutoSendDataCheckBox.IsChecked.Value)
-                    AutoSendDataSettings.IsEnabled = false;
-            }
-        }
-        private void OK_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Properties.Settings.Default.WebAppUrl = WebAppAddressTextBox.Text;
-            Properties.Settings.Default.AutoSendDataToWebApp = AutoSendDataCheckBox.IsChecked.Value;
-            Properties.Settings.Default.AutoSendDataEvery = CalculateTimeToAutoSendData();
-            Properties.Settings.Default.EnableDebugInfo = EnableDebugInfoCheckBox.IsChecked.Value;
-
-            Properties.Settings.Default.Save();
-            DialogResult = true;
-        }
-        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
-        {
-            DialogResult = false;
-        }
-        private TimeSpan CalculateTimeToAutoSendData()
-        {
-            switch (AutoSendDataComboBox.SelectedIndex)
-            {
-                case 0:
-                    return new TimeSpan(0, 30, 0);
-                case 1:
-                    return new TimeSpan(1, 0, 0);
-                case 2:
-                    return new TimeSpan(2, 0, 0);
-                case 3:
-                    return new TimeSpan(5, 0, 0);
-                case 4:
-                    return new TimeSpan(16, 0, 0);
-                case 5:
-                    return new TimeSpan(1, 0, 0, 0);
-                case 6:
-                    return new TimeSpan(3, 0, 0, 0);
-                case 7:
-                    return new TimeSpan(7, 0, 0, 0);
-                default:
-                    return new TimeSpan(1, 0, 0, 0);
-            }
+            DataContext = new ApplicationSettingsViewModel(this);
         }
     }
 }
