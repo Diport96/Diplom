2f371af8754c76913fb91a3278ed941f0021ad67
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-07-01 15:50:26 +0300

    Moving test classes to a new namespace

---
+++ DiplomApp.Core.Tests/IntegrateTests/ServerDeviceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DiplomApp.Server;
+using DiplomApp.Server.SetOfConstants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json;
+
+namespace DiplomApp.Core.Tests.IntegrateTests
+{
+    [TestClass]
+    public class ServerDeviceTests
+    {
+        [TestMethod]
+        public async Task Instance_RunAsyncIsRunCheck_TrueReturned()
+        {
+            var server = ServerDevice.Instance;
+            await server.RunAsync();
+            Assert.IsTrue(server.IsRun);
+        }
+
+        [TestMethod]
+        public async Task Instance_StopAsyncIsRunCheck_FalseReturned()
+        {
+            var server = ServerDevice.Instance;
+            await server.RunAsync();
+            await server.StopAsync();
+            Assert.IsFalse(server.IsRun);
+        }       
+    }
+}
