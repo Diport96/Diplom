93350c9ff53783840ada2bce8a4d4a559cd195cb
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-07-07 12:42:44 +0300

    Cteated unit tests for mqtt client and server

--- DiplomApp.Core.Tests/UnitTests/MqttClientTests.cs
+++ DiplomApp.Core.Tests/UnitTests/MqttClientTests.cs
@@ -10,15 +10,17 @@ namespace DiplomApp.Core.Tests.UnitTests
     public class MqttClientTests
     {
         [TestMethod]
-        public async Task IsRunProperty_InvokeRunAsyncMethod_TrueReturned()
+        public async Task IsRunProperty_RunAndStopClient_TrueAfterRunAndFalseAfterStop()
         {
             IMqttClient fakeClient = Mock.Of<IMqttClient>();
             IMqttClientOptions fakeClientOptions = Mock.Of<IMqttClientOptions>();
-
             Server.MqttClient mqttClient = new Server.MqttClient(fakeClient, fakeClientOptions);
-            await mqttClient.RunAsync();
 
+            await mqttClient.RunAsync();
             Assert.IsTrue(mqttClient.IsRun);
+
+            await mqttClient.StopAsync();
+            Assert.IsFalse(mqttClient.IsRun);
         }
     }
 }
---
+++ DiplomApp.Core.Tests/UnitTests/MqttServerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomApp.Core.Tests.UnitTests
+{
+    [TestClass]
+    public class MqttServerTests
+    {
+        [TestMethod]
+        public async Task IsRunProperty_RunAndStopServer_TrueAfterRunAndFalseAfterStop()
+        {
+            IMqttServer fakeServer = Mock.Of<IMqttServer>();
+            IMqttServerOptions fakeServerOptions = Mock.Of<IMqttServerOptions>();
+            Server.MqttServer mqttServer = new Server.MqttServer(fakeServer, fakeServerOptions);
+
+            await mqttServer.RunAsync();
+            Assert.IsTrue(mqttServer.IsRun);
+
+            await mqttServer.StopAsync();
+            Assert.IsFalse(mqttServer.IsRun);
+        }
+    }
+}
