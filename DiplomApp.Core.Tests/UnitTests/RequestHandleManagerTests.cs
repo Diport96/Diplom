c7b08d0760a599b2e15cbfaf7b84d108192d8711
Дмитрий Кудрявцев <diport_96@outlook.com>
2019-08-02 09:19:42 +0300

    Created tests for Request HandleManager

---
+++ DiplomApp.Core.Tests/UnitTests/RequestHandleManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using DiplomApp.Server.RequsestHandlers;
+using DiplomApp.Server.SetOfConstants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DiplomApp.Core.Tests.UnitTests
+{
+    [TestClass]
+    public class RequestHandleManagerTests
+    {
+        [TestMethod]
+        public void GetRequestHandler_TryGetConnectHandler_ConnectHandlerReturned()
+        {
+            Dictionary<string, string> request = new Dictionary<string, string>()
+            {
+                {"Message_Type", MessageTypes.REQUSET_TO_CONNECT }
+            };
+
+            var handleManager = new RequestHandleManager();
+            var handler = handleManager.GetRequestHandler(request);
+
+            Assert.IsInstanceOfType(handler, typeof(ConnectHandler));
+        }
+    }
+}
