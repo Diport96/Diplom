e38d8073c0e8375a3049b88fc1021e294a716604
Дмитрий Кудрявцев <diport_96@outlook.com>
2020-12-23 21:51:38 +0300

    Fix typo in thermometer's name

---
+++ DiplomApp/Models/Thermometer.cs
@@ -0,0 +1,10 @@
+﻿namespace DiplomApp.Models
+{
+    class Thermometer : Sensor
+    {
+        public Thermometer(string id, string name, double value) : base(id, name, value)
+        {
+
+        }
+    }
+}
